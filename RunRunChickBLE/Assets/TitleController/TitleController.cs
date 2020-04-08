7ddba7923b3caca2823e70145b51f5bf08237450
Ci <bear20305f@gmail.com>
2020-04-08 23:32:47 +0800

    Chick Mod input & Title Design

---
+++ RunRunChickBLE/Assets/TitleController/TitleController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    [Header("")]
+    public TitleController instance;
+
+    [Header("金幣顯示物件")]
+    public GameObject CoinObj;
+
+    [Header("空白的山羊按鈕")]
+    public GameObject GoatBtn;
+
+    [Header("確認選單-更換角色模組")]
+    public GameObject SellFalseTurePanelObj;
+    [Header("確認腳色模組選單的是否按鈕")]
+    public GameObject SellTrueBtn, SellFalseBtn;
+
+    [Header("確認選單-更換場景模組")]
+    public GameObject LevelFalseTurePanelObj;
+    [Header("確認場景選單的是否按鈕")]
+    public GameObject LevelSellTrueBtn, LevelSellFalseBtn;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) { instance = this; }
+
+        CoinObj                     = GameObject.Find("CoinPanelObj");
+
+        LevelFalseTurePanelObj      = GameObject.Find("LevelFalseTurePanelObj");
+        LevelSellTrueBtn            = GameObject.Find("LevelTrueButton");
+        LevelSellFalseBtn           = GameObject.Find("LevelFalseButton");
+
+        SellFalseTurePanelObj       = GameObject.Find("SellFalseTurePanelObj");
+        SellTrueBtn                 = GameObject.Find("SellTrueButton");
+        SellFalseBtn                = GameObject.Find("SellFalseButton");
+
+        LevelFalseTurePanelObj      .SetActive(false);
+        SellFalseTurePanelObj       .SetActive(false);
+        CoinObj                     .SetActive(false);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RunRunChickBLE/Assets/forest/day_night_switch/DayNightSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightSwitch : MonoBehaviour
+{
+    public Animation WorldLight;                                        //燈光變暗
+    public AnimationClip[] SwitchAnim = new AnimationClip[2];
+    public Material[] DayNight = new Material[2];                       //夜晚天空
+    public GameObject ChickenLight;                                     //小雞區域光
+    public Skybox WorldSkybox;
+    public bool SwitchFlag=false;
+    public Image thisIcon;
+    public Sprite DaySun;
+    public Sprite NightMoon;
+
+
+    public void ToSwitch() {
+        if (this.gameObject.GetComponent<Scrollbar>().value == 1)
+        {
+            WorldLight.clip = SwitchAnim[1];
+            WorldLight.Play();
+            ChickenLight.SetActive(true);
+            WorldSkybox.material = DayNight[1];
+            SwitchFlag = true;
+            thisIcon.sprite = NightMoon; 
+        }
+        else if (this.gameObject.GetComponent<Scrollbar>().value == 0)
+        {
+            WorldLight.clip = SwitchAnim[0];
+            WorldLight.Play();
+            WorldLight.clip = SwitchAnim[0];
+            ChickenLight.SetActive(false);
+            WorldSkybox.material = DayNight[0];
+            SwitchFlag = false;
+            thisIcon.sprite = DaySun;
+        }
+        else { }
+      
+    }
+}
